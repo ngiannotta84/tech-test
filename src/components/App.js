55455530ef372cae7aeefe433e9bd750aafc1206
nicola giannotta <nicolagiannotta84@gmail.com>
2022-09-26 10:46:00 +0100

    tech test set up

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/app.css";
+import Search from "./Search";
+
+const App = () => {
+  return (
+    <div className="app">
+      <img className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasaLogo"/>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/search.css";
+
+const Search = () => {
+    return (
+        <>
+        <input className="search-input" type="text" />
+        </>
+    )
+}
+
+export default Search;
