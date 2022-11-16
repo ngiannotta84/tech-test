7c56cdfb1234f9bc37885e145bde8fbee7efece5
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-16 17:47:24 +0000

    get images request created and amended to get just a list of images from the endpoint

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import getImages from "../requests/getImages";
 import "../styles/search.css";
 
 const Search = () => {
+    const [value, setValue] = useState("") /*This set the state of the intial value at "" 
+    then we will need an event listener which is the onChange in the input*/
+    const handleChange = (event) => setValue(event.target.value)/* and then we will need 
+    a function (eventhandler) that sets the new value as the new state remember we need 
+    to declare the value */
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        getImages(value);
+    }/*again this is an event listener on Submit that detects a change in state  
+    and is triggering and event handler that is making a request getImages that 
+    requires a param that is the value that is  also value={query}*/
     return (
         <>
-        <input className="search-input" type="text" />
+        <form className="search-form" onSubmit={handleSubmit}>
+             <input className="search-input" type="text" onChange={handleChange} value ={value}/>
+            <button className="search-btn" type="submit">Lets Go!</button>
+        </form>
         </>
     )
 }
---
+++ src/requests/getImages.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const getImages = (query) => {
+  if (!query) {
+    return Promise.resolve([]);
+  } else {
+    return axios
+      .get(`https://images-api.nasa.gov/search?q=${query}`)
+      .then((response) => {
+        const imageResults = response.data.collection.items
+       const ParsedImages= imageResults.filter(image => image.data[0].media_type === "image")
+       const images = ParsedImages.map(image => image.links[0].href)
+       console.log(images)
+       return images 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+};
+
+export default getImages;
