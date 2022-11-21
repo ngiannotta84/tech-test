1deac88ddb22132249a0b585ff667cd7cb1b734e
nicola giannotta <nicolagiannotta84@gmail.com>
2022-11-21 09:33:44 +0000

    Search Resulys component updated with second condition pointing to a map over the the href coming from the API

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,15 +4,18 @@ import React from "react";
 import "../styles/search-results.css";
 
 const SearchResults = ({results}) => {
-    if (!results.length)
+    if (!results.length) {/*here we are making sure that the search has been completed before dispaying an 
+    image so I thinkk it means the promise needs to be completed before moving on....doing this the image gets dispalyed 
+    after we search and click and not constantly as before*/
   return <p>No Results</p>
-  else {
+} else {
     return (
-      <img
-        className="card-image"
-        src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        alt="space-image"
-      />
+      <>
+        {results.map((image) => (
+          <img className="card-image" src={image} alt="spaceImage" /> /* results is an array of href thats why you have {image} in the src*/
+        ))}
+      </>
+
       );
     };
 };
--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -8,8 +8,8 @@ const getImages = (query) => {
       .get(`https://images-api.nasa.gov/search?q=${query}`)
       .then((response) => {
         const imageResults = response.data.collection.items
-       const ParsedImages= imageResults.filter(image => image.data[0].media_type === "image")
-       const images = ParsedImages.map(image => image.links[0].href)
+       const ParsedImages= imageResults.filter(image => image.data[0].media_type === "image")/*filters first object in data array for each image and gets the mediatype image */
+       const images = ParsedImages.map(image => image.links[0].href) /* it creates an array of href from the first object in the links array of the previous filtered imagaes */
        console.log(images)
        return images 
       })
